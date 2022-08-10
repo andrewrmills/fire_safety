3fcff09649657dcfa903f68cad1757510cc3c449
andrewrmills <andrewmills2515@gmail.com>
2022-08-10 15:56:38 +1200

    Added tooltips

--- script/script.js
+++ script/script.js
@@ -1,40 +1,70 @@
-// Show Bedroom
+// Show Tooltip
+$(document).ready(function(){
+  $(".bedroom").hover(function(){
+    $(".bedroomToolTip").toggle();
+  });
+});
+$(document).ready(function(){
+  $(".kitchen").hover(function(){
+    $(".kitchenToolTip").toggle();
+  });
+});
+$(document).ready(function(){
+  $(".winter").hover(function(){
+    $(".winterToolTip").toggle();
+  });
+});
+$(document).ready(function(){
+  $(".escape").hover(function(){
+    $(".escapeToolTip").toggle();
+  });
+});
+$(document).ready(function(){
+  $(".smokeAlarms").hover(function(){
+    $(".smokeAlarmsToolTip").toggle();
+  });
+});
+$(document).ready(function(){
+  $(".bbq").hover(function(){
+    $(".bbqToolTip").toggle();
+  });
+});
+// Show content Divs
 $(document).ready(function(){
     $(".bedroom").click(function(){
       $(".bedroomDiv").animate({right: '0px'});
-    //   $(".bedroomDiv").css("display", "block")
       $(".bedroomDiv").toggle();
     });
   });
-// Show Kitchen
+
   $(document).ready(function(){
     $(".kitchen").click(function(){
       $(".kitchenDiv").animate({right: '0px'});
       $(".kitchenDiv").toggle();
     });
   });
-  // Show Laundry
+
   $(document).ready(function(){
     $(".winter").click(function(){
       $(".winterDiv").animate({right: '0px'});
       $(".winterDiv").toggle();
     });
   });
-  // Show Escape
+
   $(document).ready(function(){
     $(".escape").click(function(){
       $(".escapeDiv").animate({right: '0px'});
       $(".escapeDiv").toggle();
     });
   });
-  // Show Smoke Alarms
+
   $(document).ready(function(){
     $(".smokeAlarms").click(function(){
       $(".smokeAlarmsDiv").animate({right: '0px'});
       $(".smokeAlarmsDiv").toggle();
     });
   });
-  //Show BBQ
+
   $(document).ready(function(){
     $(".bbq").click(function(){
       $(".bbqDiv").animate({right: '0px'});
