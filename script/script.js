75925d9d48f7aeeb1b183b54b3bb78dd53afd9fb
andrewrmills <andrewmills2515@gmail.com>
2022-11-05 08:59:35 +1300

    Responsivness

--- script/script.js
+++ script/script.js
@@ -1,84 +1,77 @@
+// Show rotate message for mobile
+// var width = $(window).width();
+// var height = $(window).height();
+
+// if (width > height) {
+//   $(document).ready(function(){
+//       $(".rotate").css("display", "none");
+//   });
+// }
+
 // Show Tooltip
 $(document).ready(function(){
   $(".bedroom").hover(function(){
     $(".bedroomToolTip").toggle();
   });
-});
-$(document).ready(function(){
+
   $(".kitchen").hover(function(){
     $(".kitchenToolTip").toggle();
   });
-});
-$(document).ready(function(){
+
   $(".winter").hover(function(){
     $(".winterToolTip").toggle();
   });
-});
-$(document).ready(function(){
+
   $(".escape").hover(function(){
     $(".escapeToolTip").toggle();
   });
-});
-$(document).ready(function(){
+
   $(".smokeAlarms").hover(function(){
     $(".smokeAlarmsToolTip").toggle();
   });
-});
-$(document).ready(function(){
+
   $(".bbq").hover(function(){
     $(".bbqToolTip").toggle();
   });
+
+  // Show content Divs
+  $(".bedroom").click(function(){
+    $(".bedroomDiv").animate({right: '0px'});
+    $(".bedroomDiv").toggle();
+  });
+
+   $(".kitchen").click(function(){
+    $(".kitchenDiv").animate({right: '0px'});
+    $(".kitchenDiv").toggle();
+  });
+
+  $(".winter").click(function(){
+    $(".winterDiv").animate({right: '0px'});
+    $(".winterDiv").toggle();
+  });
+
+  $(".escape").click(function(){
+    $(".escapeDiv").animate({right: '0px'});
+    $(".escapeDiv").toggle();
+  });
+
+  $(".smokeAlarms").click(function(){
+    $(".smokeAlarmsDiv").animate({right: '0px'});
+    $(".smokeAlarmsDiv").toggle();
+  });
+
+  $(".bbq").click(function(){
+    $(".bbqDiv").animate({right: '0px'});
+    $(".bbqDiv").toggle();
+  });
+
+  // Hide displayed content again
+  $(".contentHead").click(function(){
+    $(".content").animate({right: '-250px'});
+    $(".content").css("display", "none")
+  });
 });
-// Show content Divs
-$(document).ready(function(){
-    $(".bedroom").click(function(){
-      $(".bedroomDiv").animate({right: '0px'});
-      $(".bedroomDiv").toggle();
-    });
-  });
-
-  $(document).ready(function(){
-    $(".kitchen").click(function(){
-      $(".kitchenDiv").animate({right: '0px'});
-      $(".kitchenDiv").toggle();
-    });
-  });
-
-  $(document).ready(function(){
-    $(".winter").click(function(){
-      $(".winterDiv").animate({right: '0px'});
-      $(".winterDiv").toggle();
-    });
-  });
-
-  $(document).ready(function(){
-    $(".escape").click(function(){
-      $(".escapeDiv").animate({right: '0px'});
-      $(".escapeDiv").toggle();
-    });
-  });
-
-  $(document).ready(function(){
-    $(".smokeAlarms").click(function(){
-      $(".smokeAlarmsDiv").animate({right: '0px'});
-      $(".smokeAlarmsDiv").toggle();
-    });
-  });
-
-  $(document).ready(function(){
-    $(".bbq").click(function(){
-      $(".bbqDiv").animate({right: '0px'});
-      $(".bbqDiv").toggle();
-    });
-  });
-  //   Hide displayed content again
-  $(document).ready(function(){
-    $(".contentHead").click(function(){
-      $(".content").animate({right: '-250px'});
-      $(".content").css("display", "none")
-    });
-  });
   // Removes lawnmower after animation
-  $('.lawnmowerUp').bind(
-    'animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', 
-    function(e) { $(this).remove(); });
+$('.lawnmowerUp').bind(
+ 'animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', 
+  function(e) { $(this).remove(); });
